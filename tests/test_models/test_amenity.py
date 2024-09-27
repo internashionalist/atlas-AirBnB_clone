5b922c083ac1efc60c1c240d60740754dabdfe6f
Nash Thames <taylor.thames@atlasschool.com>
2024-09-27 02:14:05 -0500

    TestAmenity.0 - base

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Unittest for Amenity class
+"""
+import unittest
+from models.amenity import Amenity
+from models.base_model import BaseModel
+
+
+class TestAmenity(unittest.TestCase):
