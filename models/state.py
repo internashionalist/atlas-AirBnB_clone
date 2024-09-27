7a67f772eceb594cc7e3886b0510c4be73c077ab
Nash Thames <taylor.thames@atlasschool.com>
2024-09-27 05:13:52 -0500

    state.0 - name

---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+State class module for AirBnB clone
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Beginning stage of State class
+
+    Attributes:
+        name (str):  state name
+    """
+    name = ""
