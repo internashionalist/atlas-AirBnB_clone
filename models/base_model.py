a43bcb75d03d6125782dcf5890670a91b0ca2ebb
Nash Thames <taylor.thames@atlasschool.com>
2024-09-27 08:41:09 -0500

    PyCode

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     Defines attributes and methods for subsequent classes
 
     Attributes:
-        id (str):               unique id for each instance (assigned by uuid)
+        id (str):               unique id for each instance
         created_at (datetime):  instance creation datetime
         updated_at (datetime):  instance update datetime
 
@@ -27,7 +27,7 @@ class BaseModel:
         Method to initialize instance
 
         Attributes:
-            id (str):               unique id for each instance (assigned by uuid)
+            id (str):               unique id for each instance
             created_at (datetime):  instance creation datetime
             updated_at (datetime):  instance update datetime
 
@@ -44,33 +44,46 @@ class BaseModel:
                     self.id = kwargs.get('id')
                 elif key == 'created_at':
                     created_text = kwargs.get('created_at')
-                    formated_text = datetime.strptime(created_text, '%Y-%m-%dT%H:%M:%S.%f')
+                    formated_text = datetime.strptime(
+                        created_text, '%Y-%m-%dT%H:%M:%S.%f')
                     self.created_at = formated_text
                 elif key == 'updated_at':
                     updated_text = kwargs.get('updated_at')
-                    formated_text = datetime.strptime(updated_text, '%Y-%m-%dT%H:%M:%S.%f')
+                    formated_text = datetime.strptime(
+                        updated_text, '%Y-%m-%dT%H:%M:%S.%f')
                     self.updated_at = formated_text
                 elif key != '__class__':
                     setattr(self, key, value)
 
     def __str__(self):
-        """Method to print string of instance"""
-        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
+        """
+        Method to print string of instance
+        """
+        return '[{}] ({}) {}'.format(
+            self.__class__.__name__, self.id, self.__dict__)
         # instructions say PRINT, but isn't __str__ supposed to return a string?
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """
+        Updates the public instance attribute updated_at
+        with the current datetime
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """
+        Returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
 
-# my_model = BaseModel(id=str(uuid.uuid4()), created_at=datetime.now().isoformat(), updated_at=datetime.now().isoformat())
+# my_model = BaseModel(id=str(uuid.uuid4()), 
+# created_at=datetime.now().isoformat(), 
+# updated_at=datetime.now().isoformat())
 # print(my_model)
 
 # my_model.name = "My First Model"
