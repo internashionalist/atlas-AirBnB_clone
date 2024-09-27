af9ddfb3ea1c62343325d74d6de0e7482fc0bbfd
Nash Thames <taylor.thames@atlasschool.com>
2024-09-27 05:10:10 -0500

    amenity.1 - simplify

--- models/amenity.py
+++ models/amenity.py
@@ -8,26 +8,9 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """
-    Defines attributes and methods for Amenity class
+    Beginning stage of Amenity class
 
     Attributes:
         name (str):  name of amenity
     """
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """
-        Method to initialize instance
-
-        Attributes:
-            name (str): name of amenity
-
-        Returns:
-            None
-        """
-        super().__init__(*args, **kwargs)  # call inherited __init__ method
-        self.name = ""  # initialize name attribute
-        if kwargs:
-            for key, value in kwargs.items():  # assign key-value pairs to attributes
-                if key == 'name':  # if key is the name
-                    self.name = kwargs.get('name')  # assign value to name attribute
