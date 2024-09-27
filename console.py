15e78adc159182004637e60838aecc97d1b0624f
Nash Thames <taylor.thames@atlasschool.com>
2024-09-27 11:30:41 -0500

    Console.0 - base

---
+++ console.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Console module for AirBnB clone
+"""
+import cmd
+import json
+import models
+import models.engine
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+from datetime import datetime
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Custom class for HBNB console
+    """
+
+    prompt = "(hbnb) "
+    storage = None
+
+    def __init__(self):
+        """
+        Initializes HBNBCommand class
+        """
+        super().__init__()
+        self.storage = models.engine.file_storage.FileStorage()
+
