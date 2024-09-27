374b9391c6ff37ebfbf7eba05666ec832e5a7c5f
Nash Thames <taylor.thames@atlasschool.com>
2024-09-27 06:17:47 -0500

    TestAmenity.2 - name

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -12,10 +12,16 @@ class TestAmenity(unittest.TestCase):
     Test for Amenity class
     """
     def setUp(self):
+        """
+        Set Up for Amenity class
+        """
         self.amenity = Amenity()
         amenity_dict = self.amenity.to_dict()
 
     def tearDown(self):
+        """
+        Tear down for Amenity class
+        """
         del self.amenity
 
     def test_init(self):
@@ -23,3 +29,11 @@ class TestAmenity(unittest.TestCase):
         Test for init method
         """
         self.assertTrue(isinstance(self.amenity, BaseModel))
+
+    def test_name(self):
+        """
+        Test for name input
+        """
+        amenity = Amenity()
+        amenity.name = "Fireplace"
+        self.assertEqual(amenity.name, "Fireplace")
