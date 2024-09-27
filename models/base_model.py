d3df316b449544b6532b0212f56821723ed49f01
Nash Thames <taylor.thames@atlasschool.com>
2024-09-27 09:54:06 -0500

    BaseModel - added storage

--- models/base_model.py
+++ models/base_model.py
@@ -25,14 +25,12 @@ class BaseModel:
             id (str):               unique id for each instance
             created_at (datetime):  instance creation datetime
             updated_at (datetime):  instance update datetime
-
-        Returns:
-            None
         """
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key == 'id':
@@ -65,6 +63,7 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
