fc1a578e6975b614d674fd64386a3347200b2308
Nash Thames <taylor.thames@atlasschool.com>
2024-09-27 07:16:41 -0500

    TestState.0

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Unittest for State class
+"""
+
+import unittest
+from models.state import State
+from models.base_model import BaseModel
+
+
+class TestState(unittest.TestCase):
+    """
+    Test for State class
+    """
+    def setUp(self):
+        """
+        SetUp for State class
+        """
+        self.state = State()
+        state_dict = self.state.to_dict()
+
+    def tearDown(self):
+        """
+        TearDown for State class
+        """
+        del self.state
+
+    def test_init(self):
+        """
+        Test for init method
+        """
+        self.assertTrue(isinstance(self.state, BaseModel))
+
+    def test_state_name(self):
+        """
+        Test for name input
+        """
+        state = State()
+        state.name = "Oklahoma"
+        self.assertEqual(state.name, "Oklahoma")
