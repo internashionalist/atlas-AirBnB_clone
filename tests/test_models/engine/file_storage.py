2934420ac9a0fa664c662668bd60509272bdc939
Nash Thames <taylor.thames@atlasschool.com>
2024-09-27 08:00:42 -0500

    FileStorage.0

---
+++ tests/test_models/engine/file_storage.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""
+FileStorage class module for AirBnB clone
+"""
+
+import json
+import os.path
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+class FileStorage:
+    """
+    Class serializes/deserializes instances to/from JSON file
+
+    Attributes:
+        __file_path (str):  path to JSON file
+        __objects (dict):   dictionary of objects
+
+    Methods:
+        all(self):          returns dictionary of objects
+        new(self, obj):     sets obj in __objects with key <obj class name>.id
+        save(self):         serializes __objects to JSON file
+        reload(self):       deserializes JSON file to __objects
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def __init__(self):
+        """
+        Initializes FileStorage class
+        """
+        self.__objects = {}
+
+    def all(self):
+        """
+        Returns dictionary of objects
+
+        Returns:
+            dict:  __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Pairs objects with keys in __objects
+
+        Attributes:
+            obj (BaseModel):  object to set in __objects
+        """
+        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
+
+    def save(self):
+        """
+        Serializes __objects to JSON file
+
+        Attributes:
+            obj_dict (dict):  dictionary to store objects
+        """
+        obj_dict = {}  # dictionary to store objects
+        for key, value in self.__objects.items():  # iterate through __objects
+            obj_dict[key] = value.to_dict()  # add object to dictionary
+        with open(self.__file_path, "w", encoding="utf-8") as file:  # open file
+            json.dump(obj_dict, file)  # write to file
+
+    def reload(self):
+        """
+        Deserializes JSON file to __objects
+
+        Attributes:
+            obj_dict (dict):  dictionary to store objects
+            obj_class (dict): dictionary of classes
+        """
+        try:
+            with open(self.__file_path, "r") as file:  # open file
+                obj_dict = json.load(file)  # load file
+            
