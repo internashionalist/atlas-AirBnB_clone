52ccdab17ec0347ea5863f7fadb7ae132d6df372
TheSnewby <stephen.j.newby@gmail.com>
2024-09-26 21:50:09 -0500

    tested using example test, fixed a few errors

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 BaseModel class module for AirBnB clone
 """
 
-import models
+#import models
 import uuid
 from datetime import datetime
 
@@ -40,7 +40,7 @@ class BaseModel:
 
     def __str__(self):
         """Method to print string of instance"""
-        print('[{}] ({}) {}'.format(self._class__, self.id. self.__dict__))
+        return '[{}] ({}) {}'.format(self.__class__, self.id, self.__dict__)
         # instructions say PRINT, but isn't __str__ supposed to return a string?
 
     def save(self):
@@ -54,5 +54,18 @@ class BaseModel:
         new_dict[self.created_at] = self.created_at.isoformat()
         new_dict[self.updated_at] = self.updated_at.isoformat()
         return new_dict
+
+# my_model = BaseModel()
+# my_model.name = "My First Model"
+# my_model.my_number = 89
+# print(my_model)
+# my_model.save()
+# print(my_model)
+# my_model_json = my_model.to_dict()
+# print(my_model_json)
+# print("JSON of my_model:")
+# for key in my_model_json.keys():
+#     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
 
 
