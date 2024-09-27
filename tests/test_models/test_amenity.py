7dc7c44425b7f7d1c7a40c4b693a05553de3735a
Nash Thames <taylor.thames@atlasschool.com>
2024-09-27 05:50:29 -0500

    TestAmenity.1 - setUp, tearDown, test_init

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -8,4 +8,18 @@ from models.base_model import BaseModel
 
 
 class TestAmenity(unittest.TestCase):
-    
+    """
+    Test for Amenity class
+    """
+    def setUp(self):
+        self.amenity = Amenity()
+        amenity_dict = self.amenity.to_dict()
+
+    def tearDown(self):
+        del self.amenity
+
+    def test_init(self):
+        """
+        Test for init method
+        """
+        self.assertTrue(isinstance(self.amenity, BaseModel))
