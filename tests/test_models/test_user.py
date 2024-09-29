265e72b4851a12451c2bf2144edbc643bf9345fa
Nash Thames <taylor.thames@atlasschool.com>
2024-09-28 20:58:10 -0500

    update user tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,6 +4,7 @@ Unittest for User class
 """
 
 import unittest
+import os
 from models.user import User
 from models.base_model import BaseModel
 
@@ -18,53 +19,76 @@ class TestUser(unittest.TestCase):
         Setup test cases
         """
         self.user = User()
-        self.user.email = ""
-        self.user.password = ""
-        self.user.first_name = ""
+        self.user.email = "jackflash@email.com"
+        self.user.password = "password"
+        self.user.first_name = "Jack"
+        self.user.last_name = "Flash"
 
     def tearDown(self):
         """
         Tear down test cases
         """
         del self.user
+        if os.path.exists("file.json"):
+            os.remove("file.json")
     
     def test_init(self):
         """
         Test for init method
         """
         self.assertTrue(isinstance(self.user, BaseModel))
+        self.assertEqual(self.user.email, "jackflash@email.com")
+        self.assertEqual(self.user.password, "password")
+        self.assertEqual(self.user.first_name, "Jack")
+        self.assertEqual(self.user.last_name, "Flash")
 
     def test_email(self):
         """
         Test for email input
         """
-        user = User()
-        user.email = "jackflash@email.com"
-        self.assertEqual(user.email, "jackflash@email.com")
+        self.user.email = "jackflash@email.com"
+        self.assertEqual(self.user.email, "jackflash@email.com")
 
     def test_password(self):
         """
         Test for password input
         """
-        user = User()
-        user.password = "password"
-        self.assertEqual(user.password, "password")
+        self.user.password = "password"
+        self.assertEqual(self.user.password, "password")
 
     def test_first_name(self):
         """
         Test for first_name input
         """
-        user = User()
-        user.first_name = "Jack"
-        self.assertEqual(user.first_name, "Jack")
+        self.user.first_name = "Jack"
+        self.assertEqual(self.user.first_name, "Jack")
 
     def test_last_name(self):
         """
         Test for last_name input
         """
-        user = User()
-        user.last_name = "Flash"
-        self.assertEqual(user.last_name, "Flash")
+        self.user.last_name = "Flash"
+        self.assertEqual(self.user.last_name, "Flash")
+
+    def test_to_dict(self):
+        """
+        Test for to_dict method
+        """
+        user_dict = self.user.to_dict()
+        self.assertIn('id', user_dict)
+        self.assertIn('created_at', user_dict)
+        self.assertIn('updated_at', user_dict)
+        self.assertIn('__class__', user_dict)
+        self.assertEqual(user_dict['__class__'], 'User')
+
+    def test_save(self):
+        """
+        Test for save method
+        """
+        old_updated_at = self.user.updated_at
+        self.user.save()
+        self.assertNotEqual(old_updated_at, self.user.updated_at)
+        self.assertGreater(self.user.updated_at, old_updated_at)
 
 if __name__ == '__main__':
     unittest.main()
