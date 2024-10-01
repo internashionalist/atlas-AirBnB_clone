e945e4a1aae6adade894245c648374df0576ce45
Nash Thames <taylor.thames@atlasschool.com>
2024-10-01 00:12:28 -0500

    simplification of tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -18,13 +18,12 @@ class TestAmenity(unittest.TestCase):
         """
         SetUp for Amenity class
         """
-        self.amenity = Amenity()
+        Fireplace = Amenity()
 
     def tearDown(self):
         """
         TearDown for Amenity class
         """
-        del self.amenity
         if os.path.exists("file.json"):
             os.remove("file.json")
 
@@ -32,47 +31,9 @@ class TestAmenity(unittest.TestCase):
         """
         Test for init method
         """
-        self.assertIsInstance(self.amenity, BaseModel)
-
-    def test_amenity_name(self):
-        """
-        Test for name input
-        """
-        self.amenity.name = "Fireplace"
-        self.assertEqual(self.amenity.name, "Fireplace")
-
-    def test_amenity_id(self):
-        """
-        Test for amenity_id input
-        """
-        self.amenity.amenity_id = "123"
-        self.assertEqual(self.amenity.amenity_id, "123")
-
-    def test_amenity_dict(self):
-        """
-        Test for to_dict method
-        """
-        amenity_dict = self.amenity.to_dict()
-        self.assertIn('id', amenity_dict)
-        self.assertIn('created_at', amenity_dict)
-        self.assertIn('updated_at', amenity_dict)
-        self.assertIn('__class__', amenity_dict)
-        self.assertEqual(amenity_dict['__class__'], 'Amenity')
-
-    def test_amenity_save(self):
-        """
-        Test for save method
-        """
-        old_updated_at = self.amenity.updated_at
-        self.amenity.save()
-        self.assertNotEqual(old_updated_at, self.amenity.updated_at)
-
-    def test_amenity_str(self):
-        """
-        Test for __str__ method
-        """
-        self.assertEqual(str(self.amenity), "[Amenity] ({}) {}".format(
-            self.amenity.id, self.amenity.__dict__))
+        Fireplace = Amenity()
+        self.assertIsEqual(type(Fireplace), Amenity)
+        self.assertEqual(Fireplace.name, "")
 
 
 if __name__ == '__main__':
